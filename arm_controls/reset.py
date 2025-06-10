f60b08d2fd8cc6366bba0380e4c4b188857f2c0f
= <sultaninstem004@gmail.com>
2025-06-10 15:12:10 -0700

    minor

--- arm_controls/reset.py
+++ arm_controls/reset.py
@@ -5,8 +5,9 @@ mc = MyCobot('/dev/ttyAMA0', 115200)
 
 def set_origin_coord(): 
     mc.send_angles([0,0,0,0,0,0], 50)
-    mc.set_gripper_state(1,50)
     while mc.is_moving(): ### Allows for the movement to finish properly
         time.sleep(0.1)
+    mc.set_gripper_state(1,50)
+    time.sleep(2)
     return
 set_origin_coord()
