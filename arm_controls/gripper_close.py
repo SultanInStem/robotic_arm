275daccbec1aeb4f3a53a94f40d3efe72f2177c1
= <sultaninstem004@gmail.com>
2025-06-10 16:11:47 -0700

    minor

--- arm_controls/gripper_close.py
+++ arm_controls/gripper_close.py
@@ -5,7 +5,7 @@ mc = MyCobot('/dev/ttyAMA0', 115200)
 
 
 def close():
-    grip_value = mc.set_gripper_value(100, 70)
+    grip_value = mc.set_gripper_state(1, 70)
     print(grip_value)
     time.sleep(4)
     return
