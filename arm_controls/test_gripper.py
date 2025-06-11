4680232c85bc83a3832ad5fbbcdbf32ce6ca2d25
= <sultaninstem004@gmail.com>
2025-06-11 14:56:04 -0700

    minor

--- arm_controls/test_gripper.py
+++ arm_controls/test_gripper.py
@@ -4,7 +4,9 @@ from pymycobot import PI_PORT, PI_BAUD
 import time 
 
 mc = MyCobot("/dev/ttyAMA0", 115200)
-mc.set_gripper_mode(0)
+
+mc.set_gripper_calibration()
+
 mc.set_gripper_state(0,80)
 time.sleep(3)
 mc.set_gripper_state(1,80)
