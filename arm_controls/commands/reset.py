b8966dda830733ad910b41397e6241b13779ced7
= <sultaninstem004@gmail.com>
2025-06-16 16:57:39 -0700

    minor

--- arm_controls/commands/reset.py
+++ arm_controls/commands/reset.py
@@ -3,11 +3,11 @@ from pymycobot.mycobot import MyCobot
 mc = MyCobot('/dev/ttyAMA0', 115200)
 # This file moves the arm back to its initial position 
 
-def set_origin_coord(): 
+def reset(): 
     mc.send_angles([0,0,0,0,0,0], 50)
     while mc.is_moving(): ### Allows for the movement to finish properly
         time.sleep(0.1)
     time.sleep(2)
     mc.set_gripper_calibration()
     return
-set_origin_coord()
+reset()
