6e152c9c9a24ae1beac7017376a6c980571405d5
= <sultaninstem004@gmail.com>
2025-06-18 16:42:34 -0700

    minor

--- arm_controls/tests/ik_test.py
+++ arm_controls/tests/ik_test.py
@@ -10,6 +10,6 @@ print(angles)
 
 # Position: x = -0.050, y = -0.148, z = 0.503
 
-# end_effector_frame = chain.forward_kinematics([0,0,0,0,0,0,0,0])
-# x, y, z = end_effector_frame[:3, 3]
-# print(f"Position: x = {x:.3f}, y = {y:.3f}, z = {z:.3f}")
+end_effector_frame = chain.forward_kinematics([0,0,0,0,0,0,0,0])
+x, y, z = end_effector_frame[:3, 3]
+print(f"Position: x = {x:.3f}, y = {y:.3f}, z = {z:.3f}")
