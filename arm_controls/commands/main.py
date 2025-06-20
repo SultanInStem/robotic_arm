34ab8562ba0deb80b70991ecd183cd5472ecc26d
= <sultaninstem004@gmail.com>
2025-06-20 16:37:25 -0700

    cleanup

--- arm_controls/commands/main.py
+++ arm_controls/commands/main.py
@@ -3,7 +3,7 @@ import os
 import time
 from pymycobot.mycobot import MyCobot 
 sys.path.append(os.path.abspath(".."))
-from utils.compute_ik import compute_ik
+from utils.main import compute_ik
 from utils.globals import PORT, BANDWIDTH
 mc = MyCobot(PORT, BANDWIDTH)
 
