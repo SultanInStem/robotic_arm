ad1ba3349aff3df9c0b2ed36b6038ddfc968cd66
= <sultaninstem004@gmail.com>
2025-06-20 16:11:08 -0700

    minor

--- arm_controls/commands/move_to_location.py
+++ arm_controls/commands/move_to_location.py
@@ -5,7 +5,9 @@ sys.path.append(os.path.abspath(".."))
 from utils.compute_ik import compute_ik
 from utils.globals import PORT, BANDWIDTH
 
-mc = MyCobot(PORT, BANDWIDTH)
 
-
-compute_ik(0,0,0.5)
+def move_to_location(point, speed): 
+    mc = MyCobot(PORT, BANDWIDTH)
+    angles = compute_ik(point)
+    if len(angles) < 6: return 
+    mc.send_angles(angles,)
--- arm_controls/utils/compute_ik.py
+++ arm_controls/utils/compute_ik.py
@@ -6,12 +6,12 @@ with warnings.catch_warnings():
     from ikpy.chain import Chain
     chain = Chain.from_urdf_file("../mycobot_320pi.urdf")
 
-def compute_ik(x=0,y=0,z=0.5): # x,y,z must be in meters 
-    angles = chain.inverse_kinematics([x,y,z])
-    distance = x**2 + y**2 + z**2 
+def compute_ik(point=[0,0,0.5]): # x,y,z must be in meters 
+    angles = chain.inverse_kinematics(point)
+    distance = point[0]**2 + point[1]**2 + point[2]**2 
     if distance > 0.5**2: 
         print("the point is out of reach")
-        return [0,0,0,0,0,0] 
+        return [] 
         
     main_angles = []
     for i in range(1, len(angles) - 1): 
