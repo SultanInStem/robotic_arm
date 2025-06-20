2d1b140f495dfc7b6dea86f29f77796d239bc230
= <sultaninstem004@gmail.com>
2025-06-19 17:46:24 -0700

    minor

--- arm_controls/tests/orientation_test.py
+++ arm_controls/tests/orientation_test.py
@@ -1,13 +1,11 @@
 from pymycobot.mycobot import MyCobot 
 from ikpy.chain import Chain
 import time
- 
 mc = MyCobot("/dev/ttyAMA0", 115200) 
 
 
 def main(): 
-    angles = [0.12, -1.70, -0.05, -0.36, -2.15, -1.6]
-    mc.send_angles(angles, 10)
+    mc.send_angles([0.12, -1.70, -0.05, -0.36, -2.15, -1.6], 20)
     while mc.is_moving(): 
         time.sleep(0.1)
     time.sleep(2)
