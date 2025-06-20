f99a14e3ed1b8928b4247450cc215e81f45cd7d4
= <sultaninstem004@gmail.com>
2025-06-19 17:48:51 -0700

    minor

--- arm_controls/tests/orientation_test.py
+++ arm_controls/tests/orientation_test.py
@@ -1,7 +1,7 @@
 from pymycobot.mycobot import MyCobot 
 from ikpy.chain import Chain
 import time
-mc = MyCobot("/dev/ttyAMA0", 115200) 
+mc = MyCobot('/dev/ttyAMA0', 115200) 
 
 
 def main(): 
@@ -11,13 +11,7 @@ def main():
     time.sleep(2)
     print("Frame of the last joint: " )
     print(mc.get_coords())
-
 main()
 
 
 
-# Position: x = -0.050, y = -0.148, z = 0.503
-
-# end_effector_frame = chain.forward_kinematics([0,0,0,0,0,0,0,0])
-# x, y, z = end_effector_frame[:3, 3]
-# print(f"Position: x = {x:.3f}, y = {y:.3f}, z = {z:.3f}")
