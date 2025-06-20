5fcacba431b69e824da07ac9f218d49ab3d3931a
= <sultaninstem004@gmail.com>
2025-06-19 17:49:11 -0700

    minor

--- arm_controls/tests/orientation_test.py
+++ arm_controls/tests/orientation_test.py
@@ -5,7 +5,7 @@ mc = MyCobot('/dev/ttyAMA0', 115200)
 
 
 def main(): 
-    mc.send_angles([0.12, -1.70, -0.05, -0.36, -2.15, -1.6], 20)
+    mc.send_angles([0.12, -1.70, -0.05, -0.36, -2.15, -1.6], 40)
     while mc.is_moving(): 
         time.sleep(0.1)
     time.sleep(2)
