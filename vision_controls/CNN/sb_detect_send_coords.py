9cce2bf8a4d686d3d1b14b6992b9ebcd7215b29d
= <sultaninstem004@gmail.com>
2025-07-17 17:32:28 -0700

    minor

--- vision_controls/CNN/sb_detect_send_coords.py
+++ vision_controls/CNN/sb_detect_send_coords.py
@@ -6,7 +6,9 @@ import torch
 
 # File path for Raspberry Pi
 COORD_FILE = "/home/agxorin3/Desktop/strawberry/strawberry_coords.txt"
-model = YOLO("yolo/my_model.pt")
+model = torch.load('yolov5m.pt', map_location='cuda' if torch.cuda.is_available() else 'cpu')
+
+
 def detect_strawberries(frame):
     """Detects strawberries using color thresholding and contour detection"""
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
