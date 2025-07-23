5d2e83980928191cb9aeb3bd0e58cf36266dbce0
= <sultaninstem004@gmail.com>
2025-07-23 14:25:57 -0700

    minor

--- torchtest.py
+++ torchtest.py
@@ -1,4 +1,8 @@
 import torch
+import torchvision
+print("TorchVision version:", torchvision.__version__)
+print("TorchVision compiled with CUDA:", torchvision._C._is_cuda_build)
+
 print("Torch built with CUDA:", torch.backends.cuda.is_built())
 print("CUDA available:", torch.cuda.is_available())
 print("CUDA version (from torch):", torch.version.cuda)
