677bab0697fb3c7ec0caf1e0f6406d5b7506ef4b
= <sultaninstem004@gmail.com>
2025-07-24 13:16:52 -0700

    readme

--- arm_controls/utils/funcs.py
+++ arm_controls/utils/funcs.py
@@ -18,5 +18,6 @@ def compute_ik(point=[0,0,0.5]): # x,y,z must be in meters
         main_angles.append(round(float(angles[i]) * (180 / math.pi), 2))
     # angles are returned in degrees 
     return main_angles
-def computer_fk(): 
+def computer_fk(angles):
+     
     pass
