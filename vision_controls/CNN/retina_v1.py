a037a06db942c848f5abcc4cc258aeaabf337031
= <sultaninstem004@gmail.com>
2025-07-24 12:08:27 -0700

    minor

--- vision_controls/CNN/retina_v1.py
+++ vision_controls/CNN/retina_v1.py
@@ -5,7 +5,7 @@ import os
 import time
 
 # Load the pre-trained YOLO model
-model = YOLO("yolo/train/weights/best.pt")  # Replace with the path to your model if not in the current directory
+model = YOLO("oculus_s/train/weights/best.pt")  # Replace with the path to your model if not in the current directory
 
 image_path = ""
 
