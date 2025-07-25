0141c802df34d9d4c86de0a2453123177451be46
= <sultaninstem004@gmail.com>
2025-07-25 11:21:13 -0700

    ssh testing

--- arm_controls/utils/globals.py
+++ arm_controls/utils/globals.py
@@ -1,5 +1,5 @@
 PORT = "/dev/ttyAMA0"
 BANDWIDTH = 115200
-NVIDIA_HOST = "168.168.10.2" #change based on ifconfig of NVIDIA
+NVIDIA_HOST = "192.168.10.2" #change based on ifconfig of NVIDIA
 NVIDIA_USER = "usr2"
 NVIDIA_PASSWORD = "fresnostate"
