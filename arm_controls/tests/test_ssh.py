f771a57a154741e1f4321014b064fb4cc7ba5a12
= <sultaninstem004@gmail.com>
2025-07-25 11:34:36 -0700

    ssh

--- arm_controls/tests/test_ssh.py
+++ arm_controls/tests/test_ssh.py
@@ -8,12 +8,12 @@ from utils.globals import NVIDIA_HOST, NVIDIA_PASSWORD, NVIDIA_USER
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(NVIDIA_HOST, username=NVIDIA_USER, password=NVIDIA_PASSWORD, look_for_keys=False)
+ssh.connect(NVIDIA_HOST, username=NVIDIA_USER, password=NVIDIA_PASSWORD, look_for_keys=False, allow_agent=False)
 COORD_FILE = "/home/usr2/Desktop/missed_me.txt"
 sftp = ssh.open_sftp()
 try:
     with sftp.open(COORD_FILE, "w") as f:
-        f.write("im back bitches")
+        f.write("hello world")
 except FileNotFoundError:
         pass
 
