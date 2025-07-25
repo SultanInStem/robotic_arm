c6aa1b23c4c2124f70dbc9ff917a3697335ef765
= <sultaninstem004@gmail.com>
2025-07-25 11:53:53 -0700

    minor

--- arm_controls/tests/test01.py
+++ arm_controls/tests/test01.py
@@ -72,9 +72,11 @@ while(True):
         print("No strawberries detected.")
         continue
     coords = np.array(coords) # convert into numpy vector 
+    print(coords)
     # coords is in end_effector_frame so we should translate it to base_frame 
     a = [0,-10, 25, 55, 0, -90, 0, 0]
     rotation_matrix = get_rotation_matrix(a)
+    print(rotation_matrix)
     straw_pos = rotation_matrix @ coords
     target_angles = compute_ik(straw_pos)
     mc.send_angles(target_angles, 20)  
