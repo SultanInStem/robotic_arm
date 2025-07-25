8f161c7db9ad64b2e30ce85c02f6462b1ea7ac5a
= <sultaninstem004@gmail.com>
2025-07-25 14:50:32 -0700

    minor

--- arm_controls/tests/ik_test.py
+++ arm_controls/tests/ik_test.py
@@ -9,25 +9,22 @@ with warnings.catch_warnings():
 
 
 
-
-pos = chain.forward_kinematics([0,0,0,0,0,0,0,0])
-print(pos)
-# target = [-0.050, -0.148, 0.503]
-# angles = chain.inverse_kinematics(target)
-# print(len(angles))
+target = [-0.4, 0, 0]
+angles = chain.inverse_kinematics(target)
+for i in range(0, len(angles)): 
+    angles[i] = round(angles[i], 2)
 # angles = angles[1:7]
+print(angles)
 
-# to go from the coordinate system of the gripper 
-# to the coordinate system of the base 
-# we must multiply a vector by the inverse of rotation matrix
 
-# Position: x = -0.050, y = -0.148, z = 0.503
+end_effector_frame = chain.forward_kinematics(angles)
+x,y,z = end_effector_frame[0][3], end_effector_frame[1][3], end_effector_frame[2][3]
 
-# end_effector_frame = chain.forward_kinematics(angles)
-# rotation = end_effector_frame[:3, :3]
-# for i in range(0, len(rotation)): 
-    # for j in range(0, len(rotation[i])): 
-        # rotation[i][j] = round(rotation[i][j], 2)
-# print(rotation)
-# v_base = rotation @ v_end 
-# print(f"Position: x = {x:.3f}, y = {y:.3f}, z = {z:.3f}")
+postion = [round(x,2), round(y,2), round(z, 2)]
+print(postion)
+
+rotation = end_effector_frame[:3, :3]
+for i in range(0, len(rotation)): 
+    for j in range(0, len(rotation[i])): 
+        rotation[i][j] = round(rotation[i][j], 2)
+# print(end_effector_frame)
--- arm_controls/utils/funcs.py
+++ arm_controls/utils/funcs.py
@@ -20,13 +20,8 @@ def compute_ik(point=[0,0,0.5]): # x,y,z must be in meters
     return main_angles
 def compute_fk(angles): # angles must contain 8 items 
     end_effector_frame = chain.forward_kinematics(angles)
-    rotation = end_effector_frame[:3, :3]
     x,y,z = end_effector_frame[0][3], end_effector_frame[1][3], end_effector_frame[2][3]
     position = [round(x,2),round(y,2),round(z,2)]
-
-    for i in range(0, len(rotation)): 
-        for j in range(0, len(rotation[i])): 
-            rotation[i][j] = round(rotation[i][j], 2)
     return position
 
 
