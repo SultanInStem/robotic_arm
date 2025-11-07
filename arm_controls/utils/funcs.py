4ad1464c2b4ab6bd0fe2f1514fcd103ab8d6d6a8
= <sultaninstem004@gmail.com>
2025-11-07 10:12:37 -0800

    test04

--- arm_controls/utils/funcs.py
+++ arm_controls/utils/funcs.py
@@ -5,7 +5,7 @@ with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     from ikpy.chain import Chain
     chain = Chain.from_urdf_file("../mycobot_320pi.urdf")
-
+### The orientaton facing the table downwards is [0,0,-1] in Z direction
 def compute_ik(point=[0,0,0.5], orientation=[0,0,-1]): # x,y,z must be in meters 
     angles = chain.inverse_kinematics(point, orientation, orientation_mode="Z")
     distance = point[0]**2 + point[1]**2 + point[2]**2 
