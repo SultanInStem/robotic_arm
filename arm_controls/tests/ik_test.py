30131d2f4011960824b314f44897982d0be289ad
= <sultaninstem004@gmail.com>
2025-12-04 15:09:28 -0800

    minor

--- arm_controls/tests/ik_test.py
+++ arm_controls/tests/ik_test.py
@@ -8,7 +8,7 @@ with warnings.catch_warnings():
 
 
 x,y,z= 0.3, 0, 0.4
-x1, y1, z1 = 0.028, -0.112, 0.114
+x1, y1, z1 = 0.023, -0.114, 0.112
 
 current_pos_of_end_effector = [x, y, z]
 orientation = [0,0,-1]
