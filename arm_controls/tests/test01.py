cb0c9d415ddfb275ba5f126d3372e7625492e272
= <sultaninstem004@gmail.com>
2025-12-05 11:34:50 -0800

    fetch_coords function

--- arm_controls/tests/test01.py
+++ arm_controls/tests/test01.py
@@ -32,7 +32,10 @@ def fetch_coordinates():
     with sftp.open(remote_file_path, 'r') as f:
         bytes = f.read()
         content = bytes.decode('utf-8')
-        print("Content: ", content)
+        coords_from_cam = [float(x) for x in content.split(",")]
+        print("Coordinates from camera: ", coords_from_cam)
+        return coords_from_cam
+    return None
 
 
 fetch_coordinates()
