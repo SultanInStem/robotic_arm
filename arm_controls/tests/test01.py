5b39ef71b11fd5cb6d31b5fff5b292708951df9a
= <sultaninstem004@gmail.com>
2025-12-05 11:29:19 -0800

    minor

--- arm_controls/tests/test01.py
+++ arm_controls/tests/test01.py
@@ -15,10 +15,10 @@ username = "usr2" # username of nvidia jetson
 password = "fresnostate" # password of nvidia jetson
 remote_file_path = "/home/usr2/robotic_arm/vision_controls/AI_model/coords_data.txt"
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 def connect_ssh(): 
+    ssh = paramiko.SSHClient()
     try:   
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(HOST, port, username, password)
         sftp = ssh.open_sftp()
         print("Connection established.")
@@ -26,13 +26,23 @@ def connect_ssh():
         print(f"Connection failed: {e}")
     return ssh, sftp
 
-connect_ssh()
+ssh, sftp = connect_ssh()
+
+def fetch_coordinates(): 
+    with sftp.open(remote_file_path, 'a') as f:
+        bytes = f.read()
+        content = bytes.decode('utf-8')
+        print("Content: ", content)
 
 
+fetch_coordinates()
 
-reset()
-while True:
-     
+# reset()
+# while True:
+#     move_to_location([0.3,-0.2,0.3], 30)
+#     time.sleep(1)
+
+
 
 
 
