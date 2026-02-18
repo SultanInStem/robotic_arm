0d870aaf0a126bd754fde11a287138a04732f6ba
= <sultaninstem004@gmail.com>
2026-02-18 11:51:45 -0800

    minor

--- arm_controls/tests/test01.py
+++ arm_controls/tests/test01.py
@@ -5,7 +5,7 @@ import paramiko
 import time
 from pymycobot.mycobot320 import MyCobot320
 sys.path.append(os.path.abspath(".."))
-from commands.main import reset, move_to_location, close_gripper, open_gripper, get_arms_orientation
+from commands.main import reset, move_to_location, close_gripper, open_gripper, get_arms_orientation, get_arms_angles
 from utils.funcs import compute_ik, compute_fk, convert_to_radians, convert_point_from_end_effector_to_base_frame
 from utils.globals import PORT, BANDWIDTH
 mc = MyCobot320(PORT, BANDWIDTH)
@@ -70,7 +70,7 @@ while True:
             move_to_location(location, 20)
 
     if scanning == False:
-        current_angles = get_arms_orientation()
+        current_angles = get_arms_angles()
         print("Current angles: ", current_angles)
         break
         base_frame_coords = convert_point_from_end_effector_to_base_frame(coords, current_angles)
