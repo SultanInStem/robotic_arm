416bd56c0fd2a48cac494ad4e4a9a151ec81ff1d
= <sultaninstem004@gmail.com>
2026-02-18 13:06:25 -0800

    minor

--- arm_controls/tests/test01.py
+++ arm_controls/tests/test01.py
@@ -13,7 +13,7 @@ with warnings.catch_warnings():
     chain = Chain.from_urdf_file("../mycobot_320pi.urdf")
 from pymycobot.mycobot320 import MyCobot320
 sys.path.append(os.path.abspath(".."))
-from commands.main import reset, move_to_location, close_gripper, open_gripper, get_arms_orientation, get_arms_angles
+from commands.main import reset, move_to_location, close_gripper, open_gripper,  get_arms_angles
 from utils.funcs import compute_ik, compute_fk, convert_to_radians, convert_point_from_end_effector_to_base_frame
 from utils.globals import PORT, BANDWIDTH
 mc = MyCobot320(PORT, BANDWIDTH)
