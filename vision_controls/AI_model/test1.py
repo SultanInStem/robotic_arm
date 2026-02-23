e3605f2c019552db2d3ac067d221ae5edfddf576
= <sultaninstem004@gmail.com>
2026-02-23 14:55:51 -0800

    minor

--- vision_controls/AI_model/test1.py
+++ vision_controls/AI_model/test1.py
@@ -10,7 +10,7 @@ model = YOLO('my_model.pt')
 
 file_path = "coords_data.txt"
 CAMERA_WIDTH_OFFSET = (50/2)*0.001 # convert mm to meters
-CAMERA_HEIGHT_OFFSET = 100*0.001
+CAMERA_HEIGHT_OFFSET = 30*0.001
 Z_OFFSET = 0.13
 DETECTION_FRAMES = 4
 delta = 0.1 # uncertain in position 
