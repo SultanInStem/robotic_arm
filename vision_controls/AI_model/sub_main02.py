4ae343771e1aad0c3e3dfca5812bdd25418276f6
= <sultaninstem004@gmail.com>
2026-03-02 16:58:13 -0800

    adjustments

--- vision_controls/AI_model/sub_main02.py
+++ vision_controls/AI_model/sub_main02.py
@@ -218,6 +218,19 @@ try:
             Y = point_3d[1] + CAMERA_HEIGHT_OFFSET
             Z = point_3d[2] + Z_OFFSET
 
+
+            if x_3d > 0: 
+                x_3d = x_3d - CAMERA_WIDTH_OFFSET  
+            else:
+                x_3d = x_3d + CAMERA_WIDTH_OFFSET   
+            if y_3d > 0:
+                y_3d = y_3d + CAMERA_HEIGHT_OFFSET
+            else:
+                y_3d = y_3d - CAMERA_HEIGHT_OFFSET
+                z_3d = z_3d - Z_OFFSET 
+
+
+
             coords_buffer.append((X, Y, Z))
             detection_count += 1
 
