d475963388be436c7be0a1ffb90e154aeb1d2bb8
= <sultaninstem004@gmail.com>
2026-03-09 18:14:16 -0700

    active_links_mask

--- arm_controls/tests/ik_test.py
+++ arm_controls/tests/ik_test.py
@@ -4,7 +4,10 @@ chain = None
 with warnings.catch_warnings(): 
     warnings.simplefilter("ignore")
     from ikpy.chain import Chain
-    chain = Chain.from_urdf_file("../mycobot_320pi.urdf")
+    chain = Chain.from_urdf_file(
+        "../mycobot_320pi.urdf", 
+        active_links_mask=[False, True, True, True, True, True, True, False]
+    )
 
 
 x,y,z= 0.3, 0.15, 0.2
