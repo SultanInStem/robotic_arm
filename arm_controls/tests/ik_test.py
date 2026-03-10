4e895dc6193fe764f27802e104192b76805aa6ba
= <sultaninstem004@gmail.com>
2026-03-09 18:41:24 -0700

    testing meidum yolo

--- arm_controls/tests/ik_test.py
+++ arm_controls/tests/ik_test.py
@@ -11,7 +11,7 @@ with warnings.catch_warnings():
 
 
 x,y,z= 0.3, 0, 0.2
-x1, y1, z1 = 0.012, 0.003, 0.055
+x1, y1, z1 = 0.035, 0.000, 0.094
 
 current_pos_of_end_effector = [x, y, z]
 orientation = [0,0,-1]
