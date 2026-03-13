53885a0305fff3d769a61d71e0253dce3d2ed439
= <sultaninstem004@gmail.com>
2026-03-13 14:54:52 -0700

    demo

--- arm_controls/tests/aps_demo.py
+++ arm_controls/tests/aps_demo.py
@@ -14,16 +14,18 @@ with warnings.catch_warnings():
 from pymycobot.mycobot320 import MyCobot320
 sys.path.append(os.path.abspath(".."))
 from commands.main import reset, move_to_location, close_gripper, open_gripper,  get_arms_angles
-from utils.funcs import compute_ik, compute_fk, convert_to_radians, convert_point_from_end_effector_to_base_frame
+from utils.funcs import compute_ik, compute_fk
 from utils.globals import PORT, BANDWIDTH
 mc = MyCobot320(PORT, BANDWIDTH)
+
+degree = 180 / math.pi
 
 # tree_angles = [1.64,-0.76,-0.52,1.41,0,0]
 
 
 def scanning_table(): 
-    initial_angles = [-0.58,  -0.864, 0, -0.685,  1.57,  0]
-    final_angles = [0.7,  -0.864, 0, -0.685,  -1.57,  0]
+    initial_angles = [-0.58 * degree,  -0.864 * degree, 0, -0.685 * degree,  1.57 * degree,  0] 
+    final_angles = [0.7 * degree,  -0.864 * degree, 0, -0.685 * degree,  -1.57 * degree,  0]
     mc.send_angles(initial_angles, 10)
     while mc.is_moving(): ### Allows for the movement to finish properly
         time.sleep(0.1)
@@ -32,6 +34,7 @@ def scanning_table():
     while mc.is_moving(): ### Allows for the movement to finish properly
         time.sleep(0.1)
     time.sleep(2)
+
 scanning_table()
 
 
