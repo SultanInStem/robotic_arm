5568debac0a3cd73297c14de720764c32b49ea2a
= <sultaninstem004@gmail.com>
2026-08-04 15:57:16 -0700

    new offset

--- vision_controls/manual_test.py
+++ vision_controls/manual_test.py
@@ -11,7 +11,7 @@ print("CAUTION: THIS SCRIPT ASSUMES A PARTICULAR ALLIGNMENT OF THE CAMERA")
 # Y = 0
 # Z = 0.5521
 CAMERA_X_OFFSET = 0.42 
-CAMERA_Y_OFFSET = 0.0155*0
+CAMERA_Y_OFFSET = 0.03
 gripper_length = 0.11
 CAMERA_Z_OFFSET = 0.75
 
@@ -256,7 +256,7 @@ try:
             ### -----------------------------------
             # APPLYING PHYSICAL CAMERA OFFSETS
             x_3d = x_3d - CAMERA_X_OFFSET
-            y_3d = -y_3d - CAMERA_Y_OFFSET
+            y_3d = -y_3d + CAMERA_Y_OFFSET
             z_3d = CAMERA_Z_OFFSET - z_3d + gripper_length
             ### -----------------------------------
 
