0105a08548698d3a8aced76a7adf69388ebfd6ba
= <sultaninstem004@gmail.com>
2026-08-07 16:57:23 -0700

    server

--- vision_controls/handeye_calibrate.py
+++ vision_controls/handeye_calibrate.py
@@ -120,7 +120,7 @@ def collect(n_target=18):
     dictionary = _get_dictionary()
     board = _get_board(dictionary)
 
-    mc = MyCobot("192.168.10.2", 9000)   # adjust port for your setup
+    mc = MyCobot("129.8.233.110", 9000)   # adjust port for your setup
 
     pipeline = rs.pipeline()
     cfg = rs.config()
