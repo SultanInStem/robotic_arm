7e3b92202df4b7a5877cd9af43fc8fa607626d7f
= <sultaninstem004@gmail.com>
2026-08-11 11:27:51 -0700

    minor

--- arm_controls/scripts/close_gripper.py
+++ arm_controls/scripts/close_gripper.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from commands.main import close_gripper
-value = input("Enter extent of the closure (0-100) ")
+value = int(input("Enter extent of the closure (0-100) "))
 if value <= 100 and value >=0:
     close_gripper()
 else: 
