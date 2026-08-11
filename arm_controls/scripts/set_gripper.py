efd5173777c69d5f00d285615ecd56d69df258e6
= <sultaninstem004@gmail.com>
2026-08-11 12:45:05 -0700

    minor

--- arm_controls/scripts/set_gripper.py
+++ arm_controls/scripts/set_gripper.py
@@ -3,6 +3,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from commands.main import set_gripper
 value = int(input("Enter value: 0=closed, 100=open "))
 if value <= 100 and value >=0:
-    set_gripper(value)
+    set_gripper(value, speed=50)
 else: 
     print("Bruh")
