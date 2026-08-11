111f3fae07ba6fc254dff50882aef35ee59471bc
= <sultaninstem004@gmail.com>
2026-08-11 11:28:32 -0700

    im retarded

--- arm_controls/scripts/close_gripper.py
+++ arm_controls/scripts/close_gripper.py
@@ -3,6 +3,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from commands.main import close_gripper
 value = int(input("Enter extent of the closure (0-100) "))
 if value <= 100 and value >=0:
-    close_gripper()
+    close_gripper(value)
 else: 
     print("Bruh")
