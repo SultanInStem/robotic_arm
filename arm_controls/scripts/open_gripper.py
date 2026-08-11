a33282c0f8728b116f6bd0312cd5e26e8eb88c78
= <sultaninstem004@gmail.com>
2026-08-10 18:27:08 -0700

    import fix

--- arm_controls/scripts/open_gripper.py
+++ arm_controls/scripts/open_gripper.py
@@ -1,6 +1,5 @@
-import sys
-import os
-sys.path.append(os.path.abspath(".."))
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from commands.main import open_gripper
 
 open_gripper()
--- arm_controls/scripts/set_angles.py
+++ arm_controls/scripts/set_angles.py
@@ -1,6 +1,5 @@
-import sys
-import os
-sys.path.append(os.path.abspath(".."))
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from commands.main import set_arms_angles
 a1 = float(input("Enter a1 in radians: "))
 a2 = float(input("Enter a2 in radians: "))
